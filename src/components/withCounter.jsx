ae20ec339ea123c71ea980c4d8ac0e8501095c7f
arriolaleo <riblarsv@gmail.com>
2022-04-01 05:56:14 +0600

    first commit

---
+++ src/components/withCounter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+const withCounter = (Component,...data) =>{
+    console.log(data)
+    return function WithCounter(){
+        const [counter,setCounter] = useState(0)
+        const multiply = () =>{
+            return counter * 100
+        }
+        return(
+            <Component counter={counter} setCounter={setCounter} multiply= {multiply}/>
+        )
+
+    }
+}
+
+export default withCounter
